96a037532831b1bd2b028dc3bc5a56ef400b2d37
Owans <amachreeowanate@gmail.com>
2019-11-06 11:21:13 +0100

    ft: setup eslint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { createServer } = require('http');
 const config = require('./config')();
 const app = require('./app')();
+
 const port = config.PORT || 3330;
 
 const server = createServer(app);
